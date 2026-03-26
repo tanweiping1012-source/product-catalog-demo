f0f1643a423569587d349b3669e2abce0f7827e7
tanweiping <tanweiping1012-source@users.noreply.github.com>
2026-03-26 15:35:25 +0800

    deploy: gh-pages

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/product-catalog-demo/' : '/',
-  plugins: [react()],
-}))
